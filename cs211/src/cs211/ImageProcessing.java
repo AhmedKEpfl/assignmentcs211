b5682b9cd4403ebd8c086b2bacc31d3a1c6550d7
Nataniel Hofer <nataniel.hofer@gmail.com>
2015-04-28 09:31:48 +0200

    improving assignment 8

--- cs211/src/cs211/ImageProcessing.java
+++ cs211/src/cs211/ImageProcessing.java
@@ -5,16 +5,24 @@ import processing.core.PApplet;
 import processing.core.PImage;
 public class ImageProcessing extends PApplet {
 	PImage img;
+	PImage intermediate1;
+	PImage intermediate2;
 	PImage result;
-	HScrollbar thresholdBar;
+	HScrollbar thresholdBar1;
+	HScrollbar thresholdBar2;
 
 	//TODO part I iv faire les deux scrollbars pour les hsb
+	//TODO faire un fix pour le sobel algorithm
 
 	public void setup() { 
 		size(800, 600);
-		thresholdBar = new HScrollbar ( this , (float)0 ,(float) 580 , (float)800 , (float)20 ) ;
+		thresholdBar1 = new HScrollbar ( this , (float)0 ,(float) 580 , (float)800 , (float)20 ) ;
+		thresholdBar2 = new HScrollbar ( this , (float)0 ,(float) 560 , (float)800 , (float)20 ) ;
 		img = loadImage("board1.jpg");
+		intermediate1 = createImage(width, height, RGB);
+		intermediate2 = createImage(width, height, RGB);
 		result = createImage(width, height, RGB);
+		
 		// create a new, initially transparent, 'result' image 
 
 		//noLoop(); // no interactive behaviour: draw() will be called only once. 
@@ -22,44 +30,72 @@ public class ImageProcessing extends PApplet {
 	public void draw() {
 
 		background(color(0,0,0));
-		//changeImage2();
-		result = sobel(img);
+		//intermediate1 = convolute(img);
+		//changeImageMinBright(intermediate1, result);
+		changeImageHueFixed(img, intermediate1);
+		result = sobel(intermediate1);
 		image(result, 0, 0);
-		thresholdBar.display();
-		thresholdBar.update();
-
+		thresholdBar1.display();
+		thresholdBar2.display();
+		thresholdBar1.update();
+		thresholdBar2.update();
 	}	
-	public void changeImage () {
-		result.loadPixels();
-		for(int i = 0; i < img.width * img.height; i++) {
-			if ( brightness(img.pixels[i]) > thresholdBar.getPos()*255) {
-				result.pixels[i]= Color.black.getRGB() ;
+	public void changeImageMaxBright (PImage src, PImage dst) {
+		dst.loadPixels();
+		for(int i = 0; i < src.width * src.height; i++) {
+			if ( brightness(src.pixels[i]) > thresholdBar1.getPos()*255) {
+				dst.pixels[i]= Color.black.getRGB() ;
 			} else {
-				result.pixels[i]= Color.white.getRGB() ;
+				dst.pixels[i]= Color.white.getRGB() ;
 			}
 
 		}
-		result.updatePixels();
+		dst.updatePixels();
+	}
+	
+	public void changeImageMinBright (PImage src, PImage dst) {
+		dst.loadPixels();
+		for(int i = 0; i < src.width * src.height; i++) {
+			if ( brightness(src.pixels[i]) < thresholdBar2.getPos()*255) {
+				dst.pixels[i]= Color.black.getRGB() ;
+			} else {
+				dst.pixels[i]= Color.white.getRGB() ;
+			}
+
+		}
+		dst.updatePixels();
 	}
 
-	public void changeImage2 () {
-		result.loadPixels();
-		for(int i = 0; i < img.width * img.height; i++) {
-			if ( hue(img.pixels[i]) > 120 && hue(img.pixels[i]) < 150){
-				result.pixels[i]= img.pixels[i];
+	public void changeImageHue (PImage src, PImage dst) {
+		dst.loadPixels();
+		for(int i = 0; i < src.width * src.height; i++) {
+			if ( hue(src.pixels[i]) > thresholdBar1.getPos() * 255 && hue(src.pixels[i]) < thresholdBar2.getPos() * 255){
+				dst.pixels[i]= Color.white.getRGB();
 			} else {
-				result.pixels[i]= Color.black.getRGB() ;
+				dst.pixels[i]= Color.black.getRGB() ;
 			}
 
 		}
-		result.updatePixels();
+		dst.updatePixels();
+	}
+	public void changeImageHueFixed (PImage src, PImage dst) {
+		dst.loadPixels();
+		for(int i = 0; i < src.width * src.height; i++) {
+			if ( hue(src.pixels[i]) > 116 && hue(src.pixels[i]) < 136){//values determined experimentally for the best
+				dst.pixels[i]= Color.white.getRGB();
+			} else {
+				dst.pixels[i]= Color.black.getRGB() ;
+			}
+
+		}
+		dst.updatePixels();
 	}
 
 	public PImage convolute(PImage img) {
-		float[][] kernel = {{ 0, 0, 0 },
-				{ 1, 0, -1 },
-				{ 0, 0, 0 }};
-		float weight = 1.f;
+		float[][] kernel = {{ 9, 12, 9 },
+				{ 12, 15, 12 },
+				{ 9, 12, 9 }};
+		float weight = 99.f;
 
 		//grayscale image
 		PImage result = createImage(img.width, img.height, ALPHA);
@@ -120,7 +156,7 @@ public class ImageProcessing extends PApplet {
 						int iAdjusted = i-N/2;
 						int jAdjusted = j-N/2;
 						if (x + iAdjusted >= 0 && x + iAdjusted < img.width && y + jAdjusted >= 0 && y + jAdjusted < img.height){
-							//on prend seulement l'alpha?
+							//on prend la brightness avec la fonction processing
 							float bright = brightness((img.pixels[(y + jAdjusted)*img.width + x + iAdjusted]));
 							hSum += bright*hKernel[j][i];
 							vSum += bright*vKernel[j][i];
@@ -136,7 +172,7 @@ public class ImageProcessing extends PApplet {
 		}
 		for(int y = 2; y < img.height -2; y++){
 			for (int x = 2; x <img.width -2; x++){
-				if (buffer[y *img.width + x] > (int)(max *0.3f)) {
+				if (buffer[y *img.width + x] > (int)(max * 0.3f)) {
 					result.pixels[y*img.width + x] = color(255);
 				}else {
 					result.pixels[y * img.width + x] = color(0);
